c3fe929e9b4839192cf8842c312a73c77dfdf8d4
.JSON(Bourne) <120073373+Brickwall72@users.noreply.github.com>
2026-09-03 21:43:41 -0600

    chore(ci): implement conditional path filtering for pdf-generator chrome setup (#88)

--- services/platform/pdf-generator/server/.puppeteerrc.js
+++ services/platform/pdf-generator/server/.puppeteerrc.js
@@ -1,8 +1,4 @@
 // File: services/platform/pdf-generator/server/.puppeteerrc.js
-
-/**
- * @type {import('puppeteer').Configuration}
- */
 export default {
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+  executablePath: '/usr/bin/chromium-browser',
 };
